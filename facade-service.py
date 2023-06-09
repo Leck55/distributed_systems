89ea3c93d6d34044216874fa1c4c3364c3e37706
Leck55 <redundantleck@gmail.com>
2023-06-09 14:27:21 +0200

    files for lab4

--- facade-service.py
+++ facade-service.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
-import requests
+import requests, json
 import uuid
 import random
+import hazelcast
 
 app = FastAPI()
 
-messages_service = "http://localhost:25004/messages"
 
 class Message(BaseModel):
     id: Optional[str] = None
@@ -15,23 +15,26 @@ class Message(BaseModel):
 
 @app.post("/")
 def handle_post(message: Optional[Message] = None):
-    if message is not None and message.msg is not None:
+    if message is not None:
+        if message.msg is None:
+            raise HTTPException(status_code=400, detail="Message not provided")
         message.id = str(uuid.uuid4())
-        port = random.randint(25001, 25003)
-        logging_service = "http://localhost:{}/logging".format(port)
+        port_log = random.randint(25001, 25003)
+        logging_service = "http://localhost:{}/logging".format(port_log)
         response = requests.post(logging_service, json=message.dict())
+        queue.put(json.dumps(message.msg))
         return {"id": message.id, "msg": message.msg}
-    else:
-        raise HTTPException(status_code=400, detail="Message not provided")
-    
+
 @app.get("/")
 def handle_get():
-    port = random.randint(25001, 25003)
-    log_response = requests.get("http://localhost:{}/logging".format(port))
-    msg_response = requests.get(messages_service)
-    return str([log_response.text, msg_response.text])
-
+    port_log = random.randint(25001, 25003)
+    log_response = requests.get(f"http://localhost:{port_log}/logging")
+    port_msg = random.randint(25004, 25005)
+    msg_response = requests.get(f"http://localhost:{port_msg}/messages")
+    return [log_response.text, msg_response.text]
 
 if __name__ == "__main__":
     import uvicorn
+    hz = hazelcast.HazelcastClient()
+    queue = hz.get_queue("message-queue").blocking()
     uvicorn.run(app, host="0.0.0.0", port=25000)
--- messages-service.py
+++ messages-service.py
@@ -1,11 +1,36 @@
 from fastapi import FastAPI
+from hazelcast import HazelcastClient
+import json
+import argparse, sys, threading
 
 app = FastAPI()
 
-@app.get('/messages')
-def static_message():
-    return "Not implemented yet"
+port_rng = [25004, 25005]
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", type=int, required=True)
+args = parser.parse_args()
+
+messages = []
+
+@app.get('/messages')
+def get_messages():
+    return messages
+
+def consume_queue(queue):
+    while True:
+        queued_message = queue.take()
+        if queued_message:
+            print('Received message: '+queued_message)
+            messages.append(queued_message)
+
+if __name__ == "__main__":
+    if args.port not in port_rng:
+        print('Invalid port. Allowed ports: 25004, 25005')
+        sys.exit(1)
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=25004)
+    hz = HazelcastClient()
+    queue = hz.get_queue("message-queue").blocking()
+    thread = threading.Thread(target=consume_queue, args=(queue,))
+    thread.start()
+    uvicorn.run(app, host="0.0.0.0", port=args.port)
